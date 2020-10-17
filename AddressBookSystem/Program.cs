1b2f97ba73ce573212ea6c551bebf6e6401b587a
Administrator <siddhiseth3009@gmail.com>
2020-10-17 18:32:02 +0530

    [Siddhi]Add. method for displaying contacts and counting contacts citywise added

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AddressBookSystem
 {
@@ -19,7 +20,7 @@ namespace AddressBookSystem
                 int loop = 1;
                 while (loop == 1)
                 {
-                    Console.WriteLine("\nSelect the option. \n1. Add new contact. \n2. Edit existing contact.\n3. Delete Contact \n4. Search By City \n5. Exit.");
+                    Console.WriteLine("\nSelect the option. \n1. Add new contact. \n2. Edit existing contact.\n3. Delete Contact \n4. Search By City \n5. Count citywise contacts \n6. Exit.");
                     int option = int.Parse(Console.ReadLine());
                     switch (option)
                     {
@@ -95,17 +96,24 @@ namespace AddressBookSystem
                         case 4:
                             Console.WriteLine("Enter city whose contacts need to be searched");
                             string city = Console.ReadLine();
-                            foreach (Contact contact in binder.SearchContactsByCity(city))
+                            foreach (Contact contact in binder.CityDictionary[city])
                             {
                                 Console.WriteLine(contact.FirstName + "\t" + contact.LastName + "\t" + contact.Address + "\t" + contact.City + "\t" + contact.State + "\t" + contact.ZipCode + "\t" + contact.PhoneNumber + "\t" + contact.Email);
                             }
                             break;
                         case 5:
+                            foreach (var key in binder.CityDictionary.Keys)
+                            {
+                                    Console.WriteLine(key + "\t" + binder.CityDictionary[key].Count);
+                            }
+                            break;
+                        case 6:
                             loop = 0;
                             break;
                     }
+                    binder.Binder[addrName] = (book.People);
+                    binder.CreateDictionary();
                 }
-                binder.Binder[addrName] = (book.People);
                 Console.WriteLine("Do you want to enter an address book. \n1. yes \n2. no");
                 result = int.Parse(Console.ReadLine());
             }
